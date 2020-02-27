e21b03ef45845c12d876b4f99f10b08451bc7d0f
MartinMulas <martin_mulas@outlook.it>
2020-02-27 12:13:05 +0100

    Primo inserimento

---
+++ cmake-build-debug/CMakeFiles/mioprogetto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mioprogetto.dir/main.c.obj"
+  "libmioprogetto.dll.a"
+  "mioprogetto.exe"
+  "mioprogetto.exe.manifest"
+  "mioprogetto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mioprogetto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Goodbye and thanks for alla the fish!\n");
+    return 0;
+}
